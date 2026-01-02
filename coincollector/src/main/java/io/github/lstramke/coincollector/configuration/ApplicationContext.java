913f7f779fac3b0d41477bfd9d83e58bce654014
Lucas Stramke <git_lstramke@icloud.com>
2026-01-02 14:19:16 +0100

    feat(#8): add logouthandler in applicationContext

--- coincollector/src/main/java/io/github/lstramke/coincollector/configuration/ApplicationContext.java
+++ coincollector/src/main/java/io/github/lstramke/coincollector/configuration/ApplicationContext.java
@@ -4,12 +4,14 @@ import io.github.lstramke.coincollector.handler.CoinHandler;
 import io.github.lstramke.coincollector.handler.CollectionHandler;
 import io.github.lstramke.coincollector.handler.GroupHandler;
 import io.github.lstramke.coincollector.handler.LoginHandler;
+import io.github.lstramke.coincollector.handler.LogoutHandler;
 import io.github.lstramke.coincollector.handler.RegistrationHandler;
 import io.github.lstramke.coincollector.services.SessionManager;
 
 public record ApplicationContext(
     SessionManager sessionManager,
     LoginHandler loginHandler,
+    LogoutHandler logoutHandler,
     RegistrationHandler registrationHandler,
     GroupHandler groupHandler,
     CollectionHandler collectionHandler,
