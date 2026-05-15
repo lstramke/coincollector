93a6c3f79bac9167fe61e3356fe22ff2d54f1d81
Lucas Stramke <git_lstramke@icloud.com>
2026-05-15 19:17:37 +0200

    chore(#36): change some styling regarding dto records

--- coincollector/src/main/java/io/github/lstramke/coincollector/model/DTOs/Requests/CreateGroupRequest.java
+++ coincollector/src/main/java/io/github/lstramke/coincollector/model/DTOs/Requests/CreateGroupRequest.java
@@ -2,6 +2,7 @@ package io.github.lstramke.coincollector.model.DTOs.Requests;
 
 import java.util.List;
 
-public record CreateGroupRequest(String name, List<String> collectionIds ) {
-    
-}
+public record CreateGroupRequest(
+    String name, 
+    List<String> collectionIds 
+) {}
